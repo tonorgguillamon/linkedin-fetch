12c79ec9a6a71c788776475981e0140ab3245d04
Tono <tonorgguillamon@gmail.com>
2025-01-09 20:51:33 +0100

    jods are now displayed as item cards in a new hmtl page. instanceso of Job created and appened

--- src/app.py
+++ src/app.py
@@ -25,16 +25,18 @@ def run_app(api):
 
     @app.route('/search_jobs', methods=['POST'])
     def search_jobs():
+        jobs = []
         keywords = request.form['keywords']
-        jobs = api.search_jobs(keywords, listed_at=30 * 24 * 60 * 60, limit=3000, offset=900)
-        job_ids = get_job_ids(jobs)
+        jobs_fetched = api.search_jobs(keywords, listed_at=1 * 24 * 60 * 60, limit=10, offset=0)
+        job_ids = get_job_ids(jobs_fetched)
         for id in job_ids:
             job_info = api.get_job(id)
-            print(scrap_job(job_info))
+            print(job_info)
+            jobs.append(Job(**scrap_job(job_info)))
             #TODO: Add job to the somewhere!
 
         print("Number of jobs found: " + str(len(jobs)))
-        return render_template('index.html', companies=None, jobs=jobs)
+        return render_template('jobs_search.html', companies=None, jobs=jobs)
 
     @app.route('/company/<int:company_id>')
     def company_detail(company_id):
